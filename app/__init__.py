b07bca9c80a96d0b43558fa7cc05db36ea5e3ad2
D45h21 <sdaren6@gmail.com>
2023-09-12 19:49:10 -0500

    delete return

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,3 @@ import azure.functions as func
 def main(msg: func.QueueMessage) -> None:
     logging.info('Python queue trigger function processed a queue item: %s',
                  msg.get_body().decode('utf-8'))
-    print("holasasasasas")
-    return "holassssss"
